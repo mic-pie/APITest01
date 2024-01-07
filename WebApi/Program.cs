6ef9308466a871ad51a4e6d9dabffcd72b629257
michal.pietrzak <michal.pietrzak@astrafox.pl>
2024-01-07 20:16:43 +0100

    Add project files.

---
+++ WebApi/Program.cs
@@ -0,0 +1,25 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
